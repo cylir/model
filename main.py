2694ed17b774a98eb525e970a3bfb33fc96e2a64
cylir <116629939+cylir@users.noreply.github.com>
2022-12-12 10:52:45 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from Pages import image_detection, realtime_detection
+from Pages import cv2
 
 
 def app():
@@ -9,12 +9,7 @@ def app():
     sidebar.markdown('<a href="https://final-year-project-6a454.web.app" style="font-size:2em;text-decoration:none;font-style:bold">Dashboard</a>', unsafe_allow_html=True)
     choice = sidebar.selectbox("Menu", menu)
     if choice == 'From Image':
-        image_detection.detect_from_image()
-    elif choice == 'From Video':
-        pass
-    else:
-        realtime_detection.detect_realtime()
-
+        plate_detection.detect_from_image()
 
 if __name__ == '__main__':
     app()
